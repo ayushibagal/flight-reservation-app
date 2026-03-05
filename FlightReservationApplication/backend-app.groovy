709fd3cb126705aa28dad94731f3f2e4de48499f
ayushibagal <ayushibagal1415@gmail.com>
2026-03-05 16:23:22 +0530

    Update backend-app.groovy

--- FlightReservationApplication/backend-app.groovy
+++ FlightReservationApplication/backend-app.groovy
@@ -6,43 +6,4 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
             }
         }
-        stage('Code-Build'){
-            steps{
-                sh '''
-                    cd FlightReservationApplication
-                    mvn clean package
-                '''
-            }
-        }
-        stage('QA-test'){
-            steps{
-                withSonarQubeEnv(installationName: 'Sonar', credentialsId: 'sonar-token') {
-                 sh '''
-                     cd FlightReservationApplication
-                     mvn sonar:sonar -Dsonar.projectKey=flight-reservation
-                 '''
-                }
-            }
-        }
-        stage('Docker-Build'){
-            steps{
-                sh '''
-                    cd FlightReservationApplication
-                    docker build -t mayurwagh/flight-reservation-pls-17:latest . 
-                    docker push mayurwagh/flight-reservation-pls-17:latest
-                    docker rmi mayurwagh/flight-reservation-pls-17:latest
-
-                '''
-            }
-        }
-        stage('Deploy'){
-            steps{
-                sh '''
-                    cd FlightReservationApplication
-                    kubectl apply -f k8s/
-                '''    
-            }
-        }
-
-    }
-}
+      
